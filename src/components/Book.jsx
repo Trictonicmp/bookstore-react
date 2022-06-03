20ec823a2af5544f1366b76d35f487d273af32cf
Trictonicmp <code_shadow@hotmail.com>
2022-06-03 00:44:20 -0500

    Refactor Book buttons to be a component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import styles from '../css/components/book.module.css';
 import BookHeader from './BookHeader';
+import BookButtonsSection from './BookButtonSections';
 import Progress from './Progress';
 
 const Book = (props) => {
@@ -10,13 +11,7 @@ const Book = (props) => {
       <div className={styles.bookSection}>
         <span className={styles.bookGenre}>Action</span>
         <BookHeader title={title} author={author} />
-        <div className={styles.buttonContainer}>
-          <button className={styles.bookButton} type="button">Comments</button>
-          <span className={styles.buttonSeparator} />
-          <button className={styles.bookButton} type="button">Remove</button>
-          <span className={styles.buttonSeparator} />
-          <button className={styles.bookButton} type="button">Edit</button>
-        </div>
+        <BookButtonsSection />
       </div>
       <div className={styles.bookSection}>
         <Progress />
