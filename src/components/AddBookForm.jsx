70e553c5c1590351bcfb055c737e45cba71f9168
Trictonicmp <code_shadow@hotmail.com>
2022-06-03 17:05:09 -0500

    Add form styles

--- src/components/AddBookForm.jsx
+++ src/components/AddBookForm.jsx
@@ -1,17 +1,19 @@
 import styles from '../css/components/addbookform.module.css';
 import formStyles from '../css/components/form.module.css';
+import buttonStyles from '../css/components/button.module.css';
 
 const AddBookForm = () => (
   <div className={`page-container ${styles.bookFormContainer}`}>
     <h2 className={styles.containerTitle}>Add new book</h2>
     <form className={styles.bookForm}>
       <input className={formStyles.input} type="text" placeholder="Book title" />
-      <select placeholder="Category">
+      <select className={formStyles.select}>
+        <option>Category</option>
         <option>Action</option>
         <option>Science Fiction</option>
         <option>Economy</option>
       </select>
-      <button type="button">Add book</button>
+      <button className={`${buttonStyles.button} ${styles.button}`} type="button">Add book</button>
     </form>
   </div>
 );
