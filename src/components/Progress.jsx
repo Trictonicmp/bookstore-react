47f58c7c9accd81cbd37d5403eb12060088e086d
Trictonicmp <code_shadow@hotmail.com>
2022-06-03 10:07:33 -0500

    Add progress bar component

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -2,7 +2,7 @@ import RadialProgressBar from './RadialProgressBar';
 
 const Progress = () => (
   <>
-    <RadialProgressBar percentage="64" />
+    <RadialProgressBar percentage="0.64" />
     <div>
       <h3>64%</h3>
       <span>Completed</span>
--- src/components/RadialProgressBar.jsx
+++ src/components/RadialProgressBar.jsx
@@ -1,16 +1,35 @@
 import PropTypes from 'prop-types';
+import styles from '../css/components/radialprogressbar.module.css';
 
 const RadialProgressBar = (props) => {
   const { percentage } = props;
+
+  const calculateProgressOffset = () => ((1 - percentage) * 32);
+
   return (
-    <div>
-      {percentage}
+    <div className={styles.percent}>
+      <svg className={styles.circlesContainer} width="100%" viewBox="-0.51 -0.5 10.9 11">
+        <circle
+          className={`${styles.circleSvg} ${styles.progressBackgorund}`}
+          cx="5"
+          cy="5"
+          r="5"
+        />
+        <circle
+          className={`${styles.circleSvg} ${styles.progressProgress}`}
+          cx="5"
+          cy="5"
+          r="5"
+          transform="rotate(-90 5 5)"
+          strokeDashoffset={calculateProgressOffset()}
+        />
+      </svg>
     </div>
   );
 };
 
 RadialProgressBar.propTypes = {
-  percentage: PropTypes.number.isRequired,
+  percentage: PropTypes.string.isRequired,
 };
 
 export default RadialProgressBar;
