d046b47fdf37e126b030437cc9d1d93a15b3cd89
Trictonicmp <code_shadow@hotmail.com>
2022-06-04 09:19:04 -0500

    Remove Book components in BookList component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,5 +1,5 @@
 import styles from '../css/components/bookslist.module.css';
-import Book from './Book';
+// import Book from './Book';
 
 const BookList = () => {
   const books = [
@@ -22,9 +22,9 @@ const BookList = () => {
   console.log(books);
   return (
     <ul className={`page-container ${styles.booksContainer}`}>
-      <Book title={books[0].title} author={books[0].author} />
+      {/* <Book title={books[0].title} author={books[0].author} />
       <Book title={books[1].title} author={books[1].author} />
-      <Book title={books[2].title} author={books[2].author} />
+      <Book title={books[2].title} author={books[2].author} /> */}
     </ul>
   );
 };
