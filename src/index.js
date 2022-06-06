0a5b4bd990e7639a959ad550814fe0a83f656eb1
Trictonicmp <code_shadow@hotmail.com>
2022-06-06 17:54:00 -0500

    Add folder structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
 import './css/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
