a54502339e7025567bc25fb0bf760c56f1ccf26b
Trictonicmp <code_shadow@hotmail.com>
2022-06-07 00:25:51 -0500

    Add ducks pattern to redux/books/boojs.js

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,6 @@
 // Action types
-const ADD_BOOK = 'ADD_BOOK';
-const REMOVE_BOOK = 'REMOVE_BOOK';
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 // Action creators
 export const addBook = (payload) => ({ type: ADD_BOOK, payload });
